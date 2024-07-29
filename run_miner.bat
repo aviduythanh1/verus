d020d651841e215e1465046feb1d3292d602d6a6
aviduythanh1 <aviduythanh@gmail.com>
2024-07-29 11:58:31 +0700

    Add files via upload

---
+++ run_miner.bat
@@ -0,0 +1,6 @@
+REM For more usage info: hellminer.exe --help
+
+REM Example SSL URL
+REM .\hellminer.exe -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
+
+.\hellminer.exe -c stratum+tcp://sg.vipor.net:5040 -u RQ8xoH72keE8mJxqMGEWBMzNoMJZNjqXW1.Rig001 -p hybrid --cpu 14
